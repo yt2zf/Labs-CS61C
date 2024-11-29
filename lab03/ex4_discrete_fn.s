09119c486b40c4a95c0b32ed431b841dbc7c8024
Yanting Wan <studyYanting@163.com>
2024-11-29 14:42:23 +0800

    finish lab03

--- lab03/ex4_discrete_fn.s
+++ lab03/ex4_discrete_fn.s
@@ -6,7 +6,12 @@
 # The return value should be stored in a0
 f:
     # Your code here
-
+    addi t0 a0 3 # t0 = index
+    mv t1 a1     # t1 = address of output array
+    slli t2 t0 2 # index * 4 -> address offset
+    add t2 t1 t2
+    lw a0 0(t2)
+    
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
     jr ra
--- lab03/ex5_factorial.s
+++ lab03/ex5_factorial.s
@@ -1,7 +1,7 @@
 .globl factorial
 
 .data
-n: .word 8
+n: .word 7
 
 .text
 # Don't worry about understanding the code in main
@@ -27,7 +27,12 @@ main:
 # The return value should be stored in a0
 factorial:
     # YOUR CODE HERE
-
+    mv t0 a0
+    li a0 1
+    loop:
+        mul a0 a0 t0
+        addi t0 t0 -1
+        bgtz t0 loop
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
     jr ra
